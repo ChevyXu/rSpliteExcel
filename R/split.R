4a6ffa3b12bfcad569f2eaf3659d42e41584e96a
ChevyXu <xch940701>
2023-12-20 17:14:18 +0800

    Revert "update5.0"
    This reverts commit ee809d4c13a5cf3a75ebbc00d8a25f6ef34e3326.

--- R/split.R
+++ R/split.R
@@ -5,7 +5,7 @@
 # importing required packages
 library(readxl)
 library(writexl)
-# library(tidyverse)
+library(tidyverse)
 
 # ==============================================================================#
 # split excel to separate sheet -------------------------------------------
@@ -62,7 +62,7 @@ multiplesheets <- function(fname) {
 #' multiplesheets_write(fname)
 multiplesheets_write <- function(fname) {
   # get the function run
-  cols <- multiplesheets(fname)
+  cols <- multiplesheets(fname) %>% unique()
 
   # create output list
   for (y in seq_along(cols)) {
@@ -71,26 +71,26 @@ multiplesheets_write <- function(fname) {
 
   # getting info about all excel sheets
   sheets <- readxl::excel_sheets(fname)
-#
-#   for (x in seq_along(sheets)) {
-#     tibble <- readxl::read_excel(fname, sheet = x) %>% as.data.frame()
-#
-#     if (colnames(tibble)[1] == "...1") {
-#       tibble <- readxl::read_excel(fname, sheet = x, skip = 1) %>% as.data.frame()
-#     }
-#
-#     match <- lapply(cols, function(z) {
-#       identical(z, colnames(tibble))
-#     }) %>%
-#       as.numeric() %>%
-#       as.data.frame() %>%
-#       rownames_to_column() %>%
-#       filter(. == 1) %>%
-#       pull(rowname)
-#
-#     # get(paste0("data_", match)) <- list(tibble)
-#     assign(paste0("data_", match), get(paste0("data_", match)) %>% append(list(tibble)))
-#   }
+
+  for (x in seq_along(sheets)) {
+    tibble <- readxl::read_excel(fname, sheet = x) %>% as.data.frame()
+
+    if (colnames(tibble)[1] == "...1") {
+      tibble <- readxl::read_excel(fname, sheet = x, skip = 1) %>% as.data.frame()
+    }
+
+    match <- lapply(cols, function(z) {
+      identical(z, colnames(tibble))
+    }) %>%
+      as.numeric() %>%
+      as.data.frame() %>%
+      rownames_to_column() %>%
+      filter(. == 1) %>%
+      pull(rowname)
+
+    # get(paste0("data_", match)) <- list(tibble)
+    assign(paste0("data_", match), get(paste0("data_", match)) %>% append(list(tibble)))
+  }
 
   # for (file in seq_along(cols)) {
   #   writexl::write_xlsx(do.call(rbind, get(paste0("data_", file))), paste0("compound_", file, ".xlsx"))
